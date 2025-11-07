59b702e0bf8e7869297624d4b2d0f9860ccd1bd9
Aller <gominzyplay@gmail.com>
2025-11-07 15:49:02 +0300

    change rate-limit

--- be-api/src/plugins/rateLimit.ts
+++ be-api/src/plugins/rateLimit.ts
@@ -1,9 +1,23 @@
 import { RateLimitOptions } from '@fastify/rate-limit';
-
+import { FastifyRequest } from 'fastify';
 import { ERRORS } from 'shared';
 
+type ParamsWithToken = { token?: string };
+
+type RequestWithToken = FastifyRequest<{
+  Params: ParamsWithToken;
+}>;
+
+const getTokenFromRequest = (request: FastifyRequest): string | undefined => {
+  const paramsToken = (request as RequestWithToken).params?.token;
+  if (paramsToken) {
+    return paramsToken;
+  }
+};
+
 export const rateLimitConfig: RateLimitOptions = {
-  max: 50,
+  max: 30,
   timeWindow: '1 minute',
+  keyGenerator: (request) => getTokenFromRequest(request) ?? request.ip,
   errorResponseBuilder: () => ({ error: ERRORS.TOO_MANY_REQUESTS }),
 };
